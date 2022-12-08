1bd2512c4cf74f06e9eb98bfbc1ecadbd97f5117
gri573 <david.dg.grimm@gmail.com>
2022-12-08 09:51:11 +0100

    2048: don't hardcode field size, winning condition

--- 2048/2048.c
+++ 2048/2048.c
@@ -2,18 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define FS 3
+#define WC 2048
+
 int ipow(int x, int n) {
 	int p = 1;
 	for (int k = 0; k < n; k++) p *= x;
 	return p;
 }
 
-int printfield(char field[4][4]) {
+int printfield(char field[FS][FS]) {
 	
-	char printstring[12][52];
-	for (int i = 0; i < 624; i++) printstring[0][i] = ' ';
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
+	char printstring[3*FS][13*FS];
+	for (int i = 0; i < 39*FS*FS; i++) printstring[0][i] = ' ';
+	for (int i = 0; i < FS; i++) {
+		for (int j = 0; j < FS; j++) {
 			int val = 1<<field[i][j];
 			char color[8] = "\033[1;40m";
 			if (val == 1) color[5] = 48;
@@ -23,33 +26,33 @@ int printfield(char field[4][4]) {
 		}
 	}
 	printf("\r");
-	for (int i = 0; i < 12; i++) {
-		char thisstring[61];
-		for (int j = 0; j < 52; j++) thisstring[j] = printstring[i][j];
+	for (int i = 0; i < 3*FS; i++) {
+		char thisstring[13*FS+9];
+		for (int j = 0; j < 13*FS; j++) thisstring[j] = printstring[i][j];
 		const char clearcol[7] = "\033[1;0m";
-		for (int j = 0; j < 6; j++) thisstring[j+52] = clearcol[j];
-		thisstring[58] = '\n';
-		thisstring[59] = '\r';
-		thisstring[60] = 0;
+		for (int j = 0; j < 6; j++) thisstring[j+13*FS] = clearcol[j];
+		thisstring[13*FS+6] = '\n';
+		thisstring[13*FS+7] = '\r';
+		thisstring[13*FS+8] = 0;
 		printf(thisstring);
 	}
 	printf("\n\r");
 	return 0;
 }
 
-int movefield(int swipeAxis, int dir, char field[4][4]) {
-	char wasCompressed[4][4] = {{0}};
+int movefield(int swipeAxis, int dir, char field[FS][FS]) {
+	char wasCompressed[FS][FS] = {{0}};
 	int changed = 0;
 	if (swipeAxis < 0 || swipeAxis > 1 || (dir != -1 && dir != 1)) return -1;
 	int ij[2];
-	for (int k = 0; k < 4; k++) {
-		for (ij[0] = 3 * (1 + dir) / 2; ij[0] != 5 * (1 - dir) / 2 - 1; ij[0] -= dir) {
-			for (ij[1] = 3 * (1 + dir) / 2; ij[1] != 5 * (1 - dir) / 2 - 1; ij[1] -= dir) {
+	for (int k = 0; k < FS; k++) {
+		for (ij[0] = (FS-1) * (1 + dir) / 2; ij[0] != (FS+1) * (1 - dir) / 2 - 1; ij[0] -= dir) {
+			for (ij[1] = (FS-1) * (1 + dir) / 2; ij[1] != (FS+1) * (1 - dir) / 2 - 1; ij[1] -= dir) {
 				int lastij[2];
 				lastij[0] = ij[0];
 				lastij[1] = ij[1];
 				lastij[swipeAxis] -= dir;
-				if (lastij[swipeAxis] < 0 || lastij[swipeAxis] > 3) continue;
+				if (lastij[swipeAxis] < 0 || lastij[swipeAxis] >= FS) continue;
 				if (field[ij[0]][ij[1]] == 0) {
 					if (field[lastij[0]][lastij[1]] != 0) {
 						field[ij[0]][ij[1]] = field[lastij[0]][lastij[1]];
@@ -70,11 +73,11 @@ int movefield(int swipeAxis, int dir, char field[4][4]) {
 }
 
 int main() {
-	char field[4][4] = {{0}};
-	char freespaces[16][2];
+	char field[FS][FS] = {{0}};
+	char freespaces[FS*FS][2];
 	int ij[2] = {0};
-	for (; ij[0] < 4; ij[0]++) for (; ij[1] < 4; ij[1]++) for (int l = 0; l < 2; l++) freespaces[4*ij[0]+ij[1]][l] = ij[l];
-	int freecount = 16;
+	for (; ij[0] < FS; ij[0]++) for (; ij[1] < FS; ij[1]++) for (int l = 0; l < 2; l++) freespaces[FS*ij[0]+ij[1]][l] = ij[l];
+	int freecount = FS*FS;
 	int changed = 1;
 	system("stty raw");
 	for (int k = 0; freecount; k++) {
@@ -114,8 +117,10 @@ int main() {
 			changed = 0;
 		}
 		freecount = 0;
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 4; j++) {
+		int maxval = 1;
+		for (int i = 0; i < FS; i++) {
+			for (int j = 0; j < FS; j++) {
+				if (1<<field[i][j] > maxval) maxval = 1<<field[i][j];
 				if (field[i][j] == 0) {
 					freespaces[freecount][0] = i;
 					freespaces[freecount][1] = j;
@@ -123,10 +128,17 @@ int main() {
 				}
 			}
 		}
+		if (maxval >= WC) {
+			system("stty cooked");
+			int score = 0;
+			for (int i = 0; i < FS; i++) for (int j = 0; j < FS; j++) if (field[i][j] > 0) score += 1<<field[i][j];
+			printf("\rYOU WON!\n\nScore: %d\n", score);
+			return 0;
+		}
 	}
 	system("stty cooked");
 	int score = 0;
-	for (int i = 0; i < 4; i++) for (int j = 0; j < 4; j++) if (field[i][j] > 0) score += 1<<field[i][j];
+	for (int i = 0; i < FS; i++) for (int j = 0; j < FS; j++) if (field[i][j] > 0) score += 1<<field[i][j];
 	printf("\rGAME OVER\n\nScore: %d\n", score);
 	return 0;
 }
